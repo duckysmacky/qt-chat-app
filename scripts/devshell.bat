9d7eee404d324d8e0b707d03c14185148016f3b6
nikdor <kriperoka@gmail.com>
2026-02-26 23:12:49 +0300

    Add CMake presets to enable CLI build, add devshell script for developer shell

---
+++ scripts/devshell.bat
@@ -0,0 +1,8 @@
+@echo off
+
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+
+set Qt6_DIR=C:\Qt\6.8.0\msvc2022_64\lib\cmake\Qt6
+set CMAKE_PREFIX_PATH=C:\Qt\6.8.0\msvc2022_64
+
+cmd
